53885ec77be251b29cb6962a2a213a197eb46181
Mohammed N <mnaser11218@gmail.com>
2024-11-30 11:11:29 -0500

    added skills component

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -58,7 +58,8 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Mohammed Naser  `} <br/> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full-Stack Developer", "QA Engineer", "Java, Spring Boot, React, MySQL" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>About Me: <br></br>Results-driven Full-Stack Software Developer and QA Engineer with an M.A. in English Literature from Brooklyn College and over 6 years of teaching experience. Combined software development expertise with QA best practices to enhance testing processes, identify and resolve bugs early, and optimize testing environments. Used deep coding knowledge to design comprehensive test cases, troubleshoot complex defects, and deliver scalable, high-quality applications that meet both functional and non-functional requirements.
+                  .</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
