60c1ec12c3fdbfefc76ab37392c9a00a0bf1a541
Mohammed N <mnaser11218@gmail.com>
2024-11-30 13:57:01 -0500

    contact form componenet

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -5,8 +5,8 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
  import { Skills } from "./components/Skills";
  import { Projects } from "./components/Projects";
-// import { Contact } from "./components/Contact";
-// import { Footer } from "./components/Footer";
+ import { Contact } from "./components/Contact";
+ import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -16,8 +16,8 @@ function App() {
        <Skills />
 
      <Projects />
-      {/* <Contact />
-      <Footer />   */}
+      <Contact />
+      <Footer />  
     </div>
   );
 }
