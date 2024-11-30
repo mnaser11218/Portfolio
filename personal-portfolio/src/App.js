530c911563ccc69d74ab5c7284b06aa6203f90bd
Mohammed N <mnaser11218@gmail.com>
2024-11-30 13:45:02 -0500

    added project card component

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
  import { Skills } from "./components/Skills";
-// import { Projects } from "./components/Projects";
+ import { Projects } from "./components/Projects";
 // import { Contact } from "./components/Contact";
 // import { Footer } from "./components/Footer";
 
@@ -15,9 +15,9 @@ function App() {
       <Banner />
        <Skills />
 
-      {/* <Projects />
-      <Contact />
-      <Footer />  */}
+     <Projects />
+      {/* <Contact />
+      <Footer />   */}
     </div>
   );
 }
