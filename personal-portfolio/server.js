9f2fa71991a39fb55a36add4ff5e4a166ec0bd76
Mohammed N <mnaser11218@gmail.com>
2024-11-30 19:36:45 -0500

    successfully connected frontend to gmail server

--- personal-portfolio/server.js
+++ personal-portfolio/server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: "mnaser11218@gmail.com",
+    pass: "zqql pljt nhwn qhef"
   },
 });
 
