eda7abec92c15fefbc566a80eb79b3395d1d6672
Mohammed N <mnaser11218@gmail.com>
2025-01-08 11:57:59 -0500

    updated skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -35,7 +35,8 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p> React | JavaScript | TypeScript | Git, GitHub | Docker | 
+                        JUnit | Postman<br></br> Java | Spring Boot | Agile Methodologies | Cypress | Jmeter<br></br> MySQL / PostgreSQL | Test Driven Development (TDD) | UI/API Testing</p> 
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
