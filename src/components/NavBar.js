41cfe54cec6b8e908bd8c2b553f6b03b9c953192
Mohammed N <mnaser11218@gmail.com>
2025-01-09 18:28:06 -0500

    update header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -40,7 +40,7 @@ export const NavBar = ()=>{
             <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
               <Container>
                 <Navbar.Brand href="/">
-                  <img src={logo} alt="Logo" />
+                <a href="https://github.com/mnaser11218"><img src={githubIcon} alt="" /></a>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                   <span className="navbar-toggler-icon"></span>
