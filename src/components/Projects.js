58a7e6115bb2d344ec9759150793a088395ab09d
Mohammed N <mnaser11218@gmail.com>
2025-01-08 21:42:30 -0500

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -121,7 +121,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+<p>ClassQuery: React, Cypress, JMeter, OpenAI API, JS, HTML, CSS, Java, SpringBoot, H2, Docker. <br></br>Spark: React, Axios, AWS S3 bucket, OpenAI API, Java, SpringBoot, H2, EC2 instance. </p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
