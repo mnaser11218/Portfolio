138d47a6b30f4cdaf9f56ea59f4b033f55cf55e0
Mohammed N <mnaser11218@gmail.com>
2025-01-09 18:31:17 -0500

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import githubIcon from "../assets/img/nav-icon-github.svg"
 
 export const Footer = () => {
   return (
@@ -15,11 +16,15 @@ export const Footer = () => {
             <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
+            {/* <div className="social-icon">
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
-            </div>
+            </div> */}
+             <div className="social-icon">
+                      <a href="https://www.linkedin.com/in/mohammed-naser123"><img src={navIcon1} alt="" /></a>
+                      <a href="https://github.com/mnaser11218"><img src={githubIcon} alt="" /></a>
+                    </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
