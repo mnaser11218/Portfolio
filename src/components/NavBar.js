381d1b2ba9acb576f07f693c2460b11802b49c96
Mohammed N <mnaser11218@gmail.com>
2025-01-09 14:36:51 -0500

    updated icons and projects cards

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
+import githubIcon from "../assets/img/nav-icon-github.svg"
 import {
   BrowserRouter as Router
 } from "react-router-dom";
@@ -13,7 +14,9 @@ export const NavBar = ()=>{
 
         const [activeLink, setActiveLink] = useState('home');
         const [scrolled, setScrolled] = useState(false);
-      
+        const handleClick = () => {
+          window.location.href = 'https://www.linkedin.com/in/mohammed-naser123';
+        };
         useEffect(() => {
           const onScroll = () => {
             if (window.scrollY > 50) {
@@ -50,12 +53,11 @@ export const NavBar = ()=>{
                   </Nav>
                   <span className="navbar-text">
                     <div className="social-icon">
-                      <a href="#"><img src={navIcon1} alt="" /></a>
-                      <a href="#"><img src={navIcon2} alt="" /></a>
-                      <a href="#"><img src={navIcon3} alt="" /></a>
+                      <a href="https://www.linkedin.com/in/mohammed-naser123"><img src={navIcon1} alt="" /></a>
+                      <a href="https://github.com/mnaser11218"><img src={githubIcon} alt="" /></a>
                     </div>
                     <HashLink to='#connect'>
-                      <button onClick={console.log("clicked lets conn")} className="vvd"><span>Let’s Connect</span></button>
+                      <button onClick={handleClick} className="vvd"><span>Let’s Connect</span></button>
                     </HashLink>
                   </span>
                 </Navbar.Collapse>
