cdbd4b0e24e66d4c936e79cbb9f46682ee6412e5
Mohammed N <mnaser11218@gmail.com>
2025-01-09 22:04:59 -0500

    updated footer icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,9 +12,9 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           {/* <MailchimpForm /> */}
-          {/* <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col> */}
+          <Col size={12} sm={6}>
+          <a href="https://github.com/mnaser11218"><img src={githubIcon} alt="" /></a>
+          </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             {/* <div className="social-icon">
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
