4c9dadb27919cdabd7c315c874842f56db6f03ec
Mohammed N <mnaser11218@gmail.com>
2025-01-09 21:27:58 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,9 +12,9 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           {/* <MailchimpForm /> */}
-          <Col size={12} sm={6}>
+          {/* <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
-          </Col>
+          </Col> */}
           <Col size={12} sm={6} className="text-center text-sm-end">
             {/* <div className="social-icon">
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
